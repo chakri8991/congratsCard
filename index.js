cb576ed969216e575a65882a7bb8083f68c315a0
chakri8991 <chakravarthi1223@gmail.com>
2023-03-23 10:20:56 +0000

    react practice

---
+++ index.js
@@ -0,0 +1,10 @@
+import "./index.css";
+
+const element = (
+  // Write your code here.
+  <div className="BgCard">
+    <h1>Congratulations</h1>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
